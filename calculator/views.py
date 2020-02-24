1d47b383a01016c82d67dfc44ac298ad6236107e
EsZe <33262870+eszepto@users.noreply.github.com>
2020-02-24 15:32:39 +0700

    create view function for calculating

--- calculator/views.py
+++ calculator/views.py
@@ -1,3 +1,46 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import CalculationLog
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+def main(request):
+    return render(request,'index.html')
+
+def calApi(request):
+
+    X = float(request.POST['X'])
+    Y = float(request.POST['Y'])
+    op = request.POST['op']
+    print(X,op,Y)
+
+    c = CalculationLog()
+    c.X = X
+    c.Y = Y
+    
+    Result = 0
+
+    if (op=='+'):
+        Result = X+Y
+        c.Result = Result
+    elif op=="-":
+        Result = X-Y
+        c.Result = Result
+    elif op=="*":
+        Result = X*Y
+        c.Result = Result
+    elif op=="/":
+        Result = X/Y
+        c.Result = Result
+    elif op=="%":
+        Result = X%Y
+        c.Result = Result
+    
+    c.save()
+    print(Result)
+    return render(request,'index.html',
+                {'X':X, 
+                 'Y':Y, 
+                 'Result':Result})
+
+
--- s6101012620113/urls.py
+++ s6101012620113/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from calculator import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('calculator/', views.main),
+    path('api/cal/', views.calApi, name='calApi')
 ]
