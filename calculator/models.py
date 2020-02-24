e8428d3db28eb17ed93e32a0084292b268654604
EsZe <33262870+eszepto@users.noreply.github.com>
2020-02-24 15:31:49 +0700

    create model and migrate

--- calculator/models.py
+++ calculator/models.py
@@ -1,3 +1,8 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+class CalculationLog(models.Model):
+    X = models.FloatField()
+    Y = models.FloatField()
+    Result = models.FloatField()
+    timestamp = models.DateTimeField('cal_time', null=True, default=datetime.datetime.now)
