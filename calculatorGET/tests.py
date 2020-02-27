3fefb2aeb5961da93d8c8f96a0b56e5970facbac
EsZe <33262870+eszepto@users.noreply.github.com>
2020-02-27 14:50:48 +0700

    write unittest for storing and getting calculationgetlog

--- calculatorGET/tests.py
+++ calculatorGET/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from .models import CalculationGetLog
+from django.test import LiveServerTestCase
+from django.test import Client
 
-# Create your tests here.
+from selenium.webdriver.common.keys import Keys
+import time
+import datetime
+
+class CalculationGetLogModelTest(LiveServerTestCase):
+    def test_can_store_and_get_log(self):
+        log1 = CalculationGetLog()
+        log1.X = 50
+        log1.op = "+"
+        log1.Y = 10
+        log1.Result = 60
+        log1.save()
+
+        self.assertEqual(CalculationGetLog.objects.all().count(), 1)
+        self.assertIn(log1, CalculationGetLog.objects.all())
